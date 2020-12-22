6f31f785244dff2045bbeb4e85d1fbb46a153cbc
logycoconut <1425795337@qq.com>
2020-12-22 17:53:11 +0800

    增加eureka-registry模板

---
+++ eureka/eureka-registry/src/main/java/com/logycoconut/cloud/Application.java
@@ -0,0 +1,17 @@
+package com.logycoconut.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author hall
+ * @date 2020/12/22
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
