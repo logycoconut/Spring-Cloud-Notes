9b37737d209ca8a3802b088c37cbefd41c5e32df
logycoconut <1425795337@qq.com>
2020-12-24 00:08:49 +0800

    加入打印日志验证负载均衡功能

--- eureka/eureka-provider/src/main/java/com/logycoconut/cloud/web/ProviderController.java
+++ eureka/eureka-provider/src/main/java/com/logycoconut/cloud/web/ProviderController.java
@@ -1,5 +1,6 @@
 package com.logycoconut.cloud.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author hall
  * @date 2020/12/22
  */
+@Slf4j
 @RestController
 @RequestMapping("user")
 public class ProviderController {
 
     @GetMapping("info")
     public String info() {
+        log.info("接收到请求");
         return "我叫张三，今年二十三";
     }
 }
