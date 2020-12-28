e448e6d689a1d7fbf46976a37ac129503ed2a290
logycoconut <1425795337@qq.com>
2020-12-28 10:47:22 +0800

    重构eureka模块

--- eureka/eureka-consumer/src/main/java/com/logycoconut/cloud/eureka/EurekaConsumerApplication.java
+++ eureka/eureka-consumer/src/main/java/com/logycoconut/cloud/eureka/EurekaConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.logycoconut.cloud;
+package com.logycoconut.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka/eureka-provider/src/main/java/com/logycoconut/cloud/eureka/web/ProviderController.java
+++ eureka/eureka-provider/src/main/java/com/logycoconut/cloud/eureka/web/ProviderController.java
@@ -1,4 +1,4 @@
-package com.logycoconut.cloud.web;
+package com.logycoconut.cloud.eureka.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
