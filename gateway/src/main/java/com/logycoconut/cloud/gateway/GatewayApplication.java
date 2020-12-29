eace5940ebfd65de14359c8ef6d0508453243b52
logycoconut <1425795337@qq.com>
2020-12-29 17:54:15 +0800

    增加Gateway模块

---
+++ gateway/src/main/java/com/logycoconut/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.logycoconut.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author hall
+ * @date 2020/12/29
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
