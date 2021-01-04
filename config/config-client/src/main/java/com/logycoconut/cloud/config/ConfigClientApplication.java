bdbacaf7c31b81eb5f060a14ab7301715bbd1507
logycoconut <1425795337@qq.com>
2021-01-04 17:56:13 +0800

    config draft

---
+++ config/config-client/src/main/java/com/logycoconut/cloud/config/ConfigClientApplication.java
@@ -0,0 +1,29 @@
+package com.logycoconut.cloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hall
+ * @date 2021/1/4
+ */
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    @Value("${user.username}")
+    private String username;
+
+    @GetMapping("/")
+    public String home() {
+        return username;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ config/config-server/src/main/java/com/logycoconut/cloud/config/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.logycoconut.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author hall
+ * @date 2021/1/4
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
