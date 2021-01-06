60fbf8bb9caf5571cf0e67f6802dd992b95c2a60
logycoconut <1425795337@qq.com>
2021-01-07 00:09:43 +0800

    增加配置自动刷新代码以及加入注册中心

--- config/config-client/src/main/java/com/logycoconut/cloud/config/ConfigClientApplication.java
+++ config/config-client/src/main/java/com/logycoconut/cloud/config/ConfigClientApplication.java
@@ -3,6 +3,8 @@ package com.logycoconut.cloud.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@RefreshScope
+@EnableEurekaClient
 public class ConfigClientApplication {
 
     @Value("${user.username}")
--- config/config-server/src/main/java/com/logycoconut/cloud/config/ConfigServerApplication.java
+++ config/config-server/src/main/java/com/logycoconut/cloud/config/ConfigServerApplication.java
@@ -3,11 +3,13 @@ package com.logycoconut.cloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author hall
  * @date 2021/1/4
  */
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
